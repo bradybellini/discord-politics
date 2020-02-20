04da1788446b24b2db843951fbfe133200b3b138
bradybellini <bbellini@pm.me>
2020-02-19 22:09:00 -0700

    deleted

--- testing.py
+++
@@ -1,6 +0,0 @@
-import httpx
-import urllib3
-
-webhook_url = "https://discordapp.com/api/webhooks/679439396780507136/2mlrIl-1Dkzy5HaRllKhcbgICH5OCTq-ICom7YLyI4nG5CNVXMSOWxqEh72zfnnx3m-o"
-data = {'content':'hello'}
-httpx.post(webhook_url,data=data)
