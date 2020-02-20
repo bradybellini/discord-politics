b85405eda4c8345f9536ca46b4c623681dd8d64e
bradybellini <bbellini@pm.me>
2020-02-20 11:49:23 -0700

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -4,7 +4,8 @@ import asyncio
 from datetime import datetime
 
 
-
+# try to make it asyn, might have to use something besides feedparser to actually fetch the data and then just use feedparser
+# to well parse it lol
 async def get_reuters():
     url = "http://feeds.reuters.com/Reuters/PoliticsNews"
     f = feedparser.parse(url)
