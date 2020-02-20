a3044bfdd1ad93738a820ed579eaa47d6813adda
bradybellini <bbellini@pm.me>
2020-02-20 16:14:40 -0700

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -1,6 +1,7 @@
 import feedparser
 import pprint
 import asyncio
+import httpx
 from datetime import datetime
 
 
@@ -8,10 +9,20 @@ from datetime import datetime
 # to well parse it lol
 async def get_reuters():
     url = "http://feeds.reuters.com/Reuters/PoliticsNews"
-    f = feedparser.parse(url)
-    print(f.status)
-    if_match = f.etag
-
-
-    f = feedparser.parse(url, etag=if_match)
-    print(f.status)
+    headers = {'user-agent': 'Elections 2020 discord bot', 'If-None-Match': ""}
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url, headers=headers)
+    # print(r.headers)
+    feed_raw_data = r.text
+    f = feedparser.parse(feed_raw_data)
+    # f.entries[1].pprint
+    print(f.entries[1]['guid']) 
+    print(f.entries[1]['title'])
+    print(f.entries[1]['published'])
+    print(f.entries[1]['link'])
+    # f = feedparser.parse(url, etag=if_match)
+    # print(f.status)
+if __name__ == "__main__":
+    current_loops = asyncio.gather(get_reuters())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(current_loops)
