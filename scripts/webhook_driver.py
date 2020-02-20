4e9398972659a6f10211dfd49e849b9df001f823
bradybellini <bbellini@pm.me>
2020-02-19 22:09:21 -0700

    Update webhook_driver.py

--- scripts/webhook_driver.py
+++ scripts/webhook_driver.py
@@ -1,48 +1,16 @@
 import httpx
 import asyncio
-from datetime import datetime
-from pytz import timezone
 
 
-def get_phx_time():
-    fmt = '%Y-%m-%d %H:%M:%S %Z'
-    phx = timezone('America/Phoenix')
-    est_raw = datetime.now(phx)
-    return est_raw.strftime(fmt)
-
-async def fetch_data_webhook(error, location, time):
-    data_template = {
-        "embeds": [
-            {
-                "title": f"ERROR",
-                "description": "this is the description",
-                "color": 16711680,
-                "fields": [
-                    {
-                        "name": "Time of Incident",
-                        "value": f"{time}"
-                    },
-                    {
-                        "name": "Location of incident",
-                        "value": f"{location}"
-                    },
-                    {
-                        "name": "What Happened?",
-                        "value": f"{error}"
-                    }
-                ],
-                "timestamp": f"{get_phx_time()}"
-            }
-        ]
-    }
+async def primary_polls_avg_webhook():
     webhook_url = "https://discordapp.com/api/webhooks/679441151014600719/uU1BZBKX5QYb_vjpDLG8A1XWddXIZjN16cg_x2A1JhD-ub_RsfcrQopK5LiCGoz03NZ_"
-    async with httpx.AsyncClient() as client:
-        await client.put(webhook_url, data=data_template)
+    payload = "{\r\n  \"embeds\": [\r\n    {\r\n      \"title\": \"There has been an error.\",\r\n      \"description\": \"There has been an error in the fivethirtyeight_primary_polls_avg() loop regarding the status of the FiveThirtyEight website.\",\r\n      \"color\": 16711680\r\n    }\r\n  ]\r\n}"
+    headers = {'Content-Type': 'application/json'}
+    client = httpx.AsyncClient()
+    await client.post(webhook_url, data=payload, headers=headers)
 
 
 if __name__ == "__main__":
-    send_error = f"There has been an error in the loop where the status code is 404"
-    send_location = "fetch_data.py in function fivethirtyeight_primary_polls_avg"
-    time = get_phx_time()
-    asyncio.run(fetch_data_webhook(send_error, send_location, time))
- 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(primary_polls_avg_webhook())
+
